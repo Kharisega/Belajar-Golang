516d5c91e6ffe5d5b67d51df2f8906e98f469067
Kharisega <kharisega17@gmail.com>
2022-04-18 21:39:09 +0700

    pembuatan hello-world dengan golang

---
+++ hello-world/fungsi.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// ! mencoba membuat lebih dari satu function
+func coba() {
+	fmt.Println("Sandy kaya orang gabut!")
+	
+}
+
+// ! cara memanggil function
+func main() {
+	coba()
+}
---
+++ hello-world/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// * Menampilkan pesan hello world
+	fmt.Println("Hello World!")
+
+	// * Menampilkan pesan dengan parameter lebih dari satu
+	fmt.Println("Hello", "world!", "how", "are", "you?")
+
+	// * menampilkan pesan tanpa ln atau enter
+	fmt.Print("Hello World!")
+}
