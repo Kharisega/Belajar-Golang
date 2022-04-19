48891e7c537e1ed75293d78bd3fa1b9067263e0b
Kharisega <kharisega17@gmail.com>
2022-04-19 11:33:26 +0700

    materi operator

---
+++ operator/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main()  {
+	// TODO Materi Operator
+	// * Operator dalam Golang ada beberapa jenis
+	// 1. Operator Aritmatika
+
+	/*
+	   ? Operator dalam Golang ada beberapa jenis
+	   * 1. Operator Aritmatika seperti + - * / %
+	   * 2. Operator Perbandingan seperti == , != , < , > , <= , >=
+	   * 3. Operator Logika seperti && , || , !
+	   * 4. Penugasan seperti = untuk pengisian, += untuk penjumlahan, -+ untuk pengurangan, *= untuk perkalian, %= untuk sisa hasil bagi
+	   *5. Operator Bitwise seperti & | ^ << >>
+	*/
+}
